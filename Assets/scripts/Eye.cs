da598dc1b56e98df19251ad9f3188a1e3bd05bcc
eminet <eminet666@gmail.com>
2021-10-01 18:00:44 +0200

    x

--- Assets/scripts/Eye.cs
+++ Assets/scripts/Eye.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Eye : MonoBehaviour
 {
 
-    public Transform target;
+    public Transform target; //public Transform target = null;
+    public bool isFalling = false; // public pour le voir dans l'inspecteur
+    public GameObject clone;
 
     // Start is called before the first frame update
     void Start()
@@ -17,9 +19,37 @@ public class Eye : MonoBehaviour
     void Update()
     {
         if(target != null){
-            transform.LookAt(target);
+            if(isFalling == false){
+                transform.LookAt(target);
+            }
+            
         }
+    }
 
+    void OnMouseDown() {
+        //clone = Instantiate(gameObject); // mais par defaut sur la scène
+        clone = Instantiate(gameObject, transform.parent); // dans le parent de l'appelant
+        clone.SetActive(false);
+
+        //Debug.Break();  // met en pause le programme
+        //target = null;  // se sépare de la target
+        isFalling = true;
+        //gameObject.AddComponent<Rigidbody>(); // ajoute un composant
+        Rigidbody body = gameObject.AddComponent<Rigidbody>();
+        // trainée angulaire = friction associée à la friction
+        // valeur à 0.2 (défaut 0.05) pour ralentir roulement
+        body.angularDrag = 0.9f;
+        Debug.Log("i’m falliiiiiiiiiing");
+
+        StartCoroutine(FallEnd());
 
     }
+
+    IEnumerator FallEnd(){
+        yield return new WaitForSeconds(4f); // c'est ici qu'on attends
+        isFalling = false;
+        Debug.Log("still alive");
+    }
+
+
 }
--- README.md
+++ README.md
@@ -19,3 +19,20 @@ GameObject clone = Instantiate(gameObject);
 Destroy(clone, 5f);
 ```
 [voir usage dans CloneOnClick](./assets/CloneOnClic.cs)
+
+## Component (accès à une instance, détruir
+Attention il peut-être "null"e, ajouter)`
+* Pour récupérer un composant existant sur un GameObject :
+`GetComponent<MonComposant>()`, par exemple : 
+```csharp
+Rigidbody body = GetComposant<RigidBody>();
+```
+Attention il peut-être "null"
+
+* Pour ajouter un composant sur un GameObject
+`gameObject.AddComponent<MonComposant>()` par exemple :
+```chsarp
+gameObject.AddComponent<RigidBody>();
+```
+
+Remarque : Ctrl-cmd+Shif+V pour afficher le readme
